b8e195234a0586ea816082cae237b265e266155b
Marisol G <Marisol G@Marisol_G>
2022-11-16 15:36:19 -0700

    EJERCICIO 1.1

--- src/github1/Github1.java
+++ src/github1/Github1.java
@@ -15,7 +15,8 @@ public class Github1 {
      */
     public static void main(String[] args) {
         System.out.println("Hola a todos ");
-        System.out.println("Marisol");
+        System.out.println("Marisol Gómez");
+        
     }
     
 }
